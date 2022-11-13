af4bb172c0bec45cfa977a8dc79ecdcc448ebf9b
MrSofty123 <shaheer1642@gmail.com>
2022-11-14 02:52:19 +0500

    chan

--- frontend/src/modules/SquarePaymentForm.js
+++ frontend/src/modules/SquarePaymentForm.js
@@ -12,6 +12,7 @@ function SquarePaymenForm() {
   return (
     <div className="App">
       <header className="App-header">
+            <div style={{backgroundColor: '#404e69', borderRadius: '20px', padding: '50px'}}>
         {(paymentStatus == 0 || paymentStatus == 500) ? 
           <PaymentForm
             /**
@@ -49,21 +50,38 @@ function SquarePaymenForm() {
              * We strongly recommend use this function to verify the buyer and reduce
              * the chance of fraudulent transactions.
              */
+            createVerificationDetails={() => ({
+              amount: '3.99',
+              /* collected from the buyer */
+              billingContact: {
+                addressLines: ['123 Main Street', 'Apartment 1'],
+                familyName: 'Doe',
+                givenName: 'John',
+                countryCode: 'GB',
+                city: 'London',
+              },
+              currencyCode: 'USD',
+              intent: 'CHARGE',
+            })}
             /**
              * Identifies the location of the merchant that is taking the payment.
              * Obtained from the Square Application Dashboard - Locations tab.
              */
             locationId="L4WPY00F9H4HC"
           >
-            <div style={{marginBottom: '20px'}}>
-              Purchase Warframe Hub VIP
-            </div>
-            <CreditCard />
+              <div style={{margin: '20px'}}>
+                Purchase Warframe Hub VIP
+              </div>
+              <div style={{margin: '20px'}}>
+                $3.99
+              </div>
+              <CreditCard />
           </PaymentForm> : <></>
         }
-        <div>
+        <div style={{margin: '20px',color:'red'}}>
           {paymentMessage}
         </div>
+            </div>
       </header>
     </div>
   );
