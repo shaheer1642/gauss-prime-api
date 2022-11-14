48a89c840e269538476724524d89fbdc8147913c
MrSofty123 <shaheer1642@gmail.com>
2022-11-14 18:43:12 +0500

    Update SquarePaymentFormSandbox.js

--- frontend/src/modules/SquarePaymentFormSandbox.js
+++ frontend/src/modules/SquarePaymentFormSandbox.js
@@ -51,19 +51,6 @@ function SquarePaymenFormSandbox() {
              * We strongly recommend use this function to verify the buyer and reduce
              * the chance of fraudulent transactions.
              */
-            createVerificationDetails={() => ({
-              /* collected from the buyer */
-              amount: '100',
-              currencyCode: 'USD',
-              billingContact: {
-                addressLines: ['123 Main Street', 'Apartment 1'],
-                familyName: 'Doe',
-                givenName: 'John',
-                countryCode: 'GB',
-                city: 'London',
-              },
-              intent: 'CHARGE',
-            })}
             /**
              * Identifies the location of the merchant that is taking the payment.
              * Obtained from the Square Application Dashboard - Locations tab.
