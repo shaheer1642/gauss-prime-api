e957bee31c6ac00e70e696e8ea0fdbd014c52057
MrSofty123 <shaheer1642@gmail.com>
2022-11-15 23:27:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.use(function (req, res, next) {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
   }
-  res.setHeader('Access-Control-Allow-Origin', origin);
+  if (origin)
+    res.setHeader('Access-Control-Allow-Origin', origin);
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
