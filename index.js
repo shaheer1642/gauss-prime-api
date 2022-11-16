59a65c602101c8702cea4ac9d92a7678fce5f525
MrSofty123 <shaheer1642@gmail.com>
2022-11-17 00:02:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(function (req, res, next) {
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
+   
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
