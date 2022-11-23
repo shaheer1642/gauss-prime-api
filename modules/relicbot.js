d47e62440803b6c3e98ef0eb3d06bb96daad71bb
MrSofty123 <shaheer1642@gmail.com>
2022-11-24 03:05:53 +0500

    Update relicbot.js

--- modules/relicbot.js
+++ modules/relicbot.js
@@ -18,16 +18,79 @@ function squadsCreate(data,callback) {
     const lines = data.message.toLowerCase().split('\n')
     Promise.all(lines.map(line => {
         return new Promise((resolve,reject) => {
-            const words = line.split(' ')
-            const tier = words[0], relic = words[1], host = data.discord_id, squad_id=uuid.v4();
-            if (!['lith','meso','neo','axi'].includes(tier)) {
-                return resolve({
-                    code: 400,
-                    message: `Invalid tier ${tier}`
-                })
-            }
-            db.query(`INSERT INTO rb_squads (squad_id,tier,relic,members,original_host,channel_id) VALUES ('${squad_id}','${tier}','${relic}','["${host}"]','${host}','${data.channel_id || main_squads_channel}')`)
-            .then(res => {
+            const squad_id = uuid.v4()
+            var str = line
+            str = str.toLowerCase().trim()
+            str = str.replace(/^h /,'').replace(/off$/g,'').replace(/off$/g,'').replace(/offcycle$/g,'').replace(/ or /g,'')
+            const tier = str.split(' ')[0]
+            if (!['lith','meso','neo','axi'].includes(tier)) return resolve({
+                code: 400,
+                message: `Invalid tier **${tier}**`
+            })
+            str = str.replaceAll(str.split(' ')[0],'').replace(/,/g,' ').replace(/\s+/g, ' ').trim()
+            const subline = str.split(' with ')
+
+            var main_relics = []
+            var main_refinements = []
+            var off_relics = []
+            var off_refinements = []
+            var squad_type = ''
+            var cycle_count = ''
+
+            subline[0].split(' ').forEach((word,index) => {
+                if (['int','flaw','rad'].includes(word)) {
+                    if (!main_refinements.includes(word)) main_refinements.push(word)
+                }
+                else if (['2b2','4b4'].includes(word)) {
+                    squad_type = word
+                }
+                else if (['2b2i','4b4i','2b2f','4b4f','2b2r','4b4r',].includes(word)) {
+                    squad_type = word.substring(0,3)
+                    const refinement = word[word.length - 1].replace('i','int').replace('f','flaw').replace('r','rad')
+                    if (!main_refinements.includes(refinement)) main_refinements.push(refinement)
+                }
+                else if ((word.length == 2 || word.length == 3) && !Number(word[0]) && Number(`${word[1]}${word[2] || ''}`)) {
+                    if (!main_relics.includes(word.toUpperCase())) main_relics.push(word.toUpperCase())
+                }
+                else if (word.match('cycle') || word.match('cycles')) {
+                    const prev_word = subline[0].split(' ')[index-1]
+                    cycle_count = prev_word
+                }
+            });
+            if (main_relics.length == 0) return resolve({
+                code: 400,
+                message: `Relic name could not be determined`
+            })
+            subline[1]?.split(' ').forEach((word,index) => {
+                if (['int','flaw','rad'].includes(word)) {
+                    if (!off_refinements.includes(word)) off_refinements.push(word)
+                }
+                else if ((word.length == 2 || word.length == 3) && !Number(word[0]) && Number(`${word[1]}${word[2] || ''}`)) {
+                    if (!off_relics.includes(word.toUpperCase())) off_relics.push(word.toUpperCase())
+                }
+                else if (word.match('cycle') || word.match('cycles')) {
+                    const prev_word = subline[1].split(' ')[index-1]
+                    cycle_count = prev_word
+                }
+            });
+
+            if (squad_type == '') squad_type = '4b4'
+            if (main_refinements.length == 0) main_refinements.push('rad')
+
+            db.query(`INSERT INTO rb_squads (squad_id,tier,members,original_host,channel_id,main_relics,main_refinements,off_relics,off_refinements,squad_type,cycle_count) 
+            VALUES 
+                ('${squad_id}',
+                '${tier}',
+                '["${data.discord_id}"]',
+                '${data.discord_id}',
+                '${data.channel_id || main_squads_channel}',
+                '${JSON.stringify(main_relics)}',
+                '${JSON.stringify(main_refinements)}',
+                '${JSON.stringify(off_relics)}',
+                '${JSON.stringify(off_refinements)}',
+                '${squad_type}',
+                '${cycle_count}')
+            `).then(res => {
                 if (res.rowCount == 1) {
                     return resolve({
                         code: 200
