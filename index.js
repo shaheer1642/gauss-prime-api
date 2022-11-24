913af54b4bbb9461388687a9e326d0cad02e9421
MrSofty123 <shaheer1642@gmail.com>
2022-11-24 21:17:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1311,7 +1311,7 @@ This trading session will be auto-closed in 15 minutes`, attachments: payload.it
     if (payload[0].members.length == 4 && payload[1].members.length < 4) {
         db.query(`
           UPDATE rb_squads SET status='opened' WHERE status = 'active' AND squad_id = '${payload[0].squad_id}';
-          UPDATE rb_squads SET members=remove_dupes(members${payload[0].members.map(discord_id => `-'${discord_id}'`).join('')}) WHERE status='active' AND squad_id != '${payload[0].squad_id}';
+          UPDATE rb_squads SET members=members${payload[0].members.map(discord_id => `-'${discord_id}'`).join('')} WHERE status='active' AND squad_id != '${payload[0].squad_id}';
         `).catch(console.error)
     }
     for (const socket in clients) {
