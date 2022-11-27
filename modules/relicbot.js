a616799c5db96f29781e977baa17b955fa86dc52
MrSofty123 <shaheer1642@gmail.com>
2022-11-28 01:28:32 +0500

    Update relicbot.js

--- modules/relicbot.js
+++ modules/relicbot.js
@@ -10,7 +10,14 @@ const endpoints = {
     'relicbot/squads/addmember': squadsAddMember,
     'relicbot/squads/removemember': squadsRemoveMember,
     'relicbot/squads/leaveall': squadsLeaveAll,
+
     'relicbot/squads/messageCreate': squadsMessageCreate,
+
+    'relicbot/trackers/create': trackersCreate,
+    'relicbot/trackers/fetch': trackersFetch,
+    'relicbot/trackers/delete': trackersDelete,
+    'relicbot/trackers/fetchSubscribers': trackersfetchSubscribers,
+
     'relicbot/users/fetch': usersFetch,
 }
 
@@ -43,103 +50,38 @@ function squadsCreate(data,callback) {
     console.log('[squadsCreate] data:',data)
     if (!data.message) return callback({code: 500, err: 'No message provided'})
     if (!data.discord_id) return callback({code: 500, err: 'No discord_id provided'})
-    const lines = data.message.toLowerCase().split('\n')
+    const lines = data.message.toLowerCase().trim().split('\n')
     Promise.all(lines.map(line => {
         return new Promise((resolve,reject) => {
             const squad_id = uuid.v4()
-            var str = line
-            str = str.toLowerCase().trim()
-            str = str.replace(/^h /,'').replace(/off$/g,'').replace(/off$/g,'').replace(/offcycle$/g,'').replace(/ or /g,'')
-            .replace(/^random lith/,'lith random').replace(/^random meso/,'meso random').replace(/^random neo/,'neo random').replace(/^random axi/,'axi random');
-            const tier = str.split(' ')[0]
-            if (!['lith','meso','neo','axi'].includes(tier)) return resolve({
+            const squad = relicBotStringToSquad(line)
+            if (!['lith','meso','neo','axi'].includes(squad.tier)) return resolve({
                 code: 400,
                 message: `Invalid tier **${tier}**`
             })
-            str = str.replaceAll(tier,'').replace(/,/g,' ').replace(/\s+/g, ' ').trim()
-            const subline = str.split(' with ')
-
-            var main_relics = []
-            var main_refinements = []
-            var off_relics = []
-            var off_refinements = []
-            var squad_type = ''
-            var cycle_count = ''
-            var is_steelpath = false
-            var is_railjack = false
-
-            subline[0].split(' ').forEach((word,index) => {
-                if (['int','flaw','rad','intact','flawless','radiant'].includes(word)) {
-                    word = word.replace('intact','int').replace('flawless','flaw').replace('radiant','rad')
-                    if (!main_refinements.includes(word)) main_refinements.push(word)
-                }
-                else if (['2b2','4b4'].includes(word)) {
-                    squad_type = word
-                }
-                else if (['2b2i','4b4i','2b2f','4b4f','2b2r','4b4r',].includes(word)) {
-                    squad_type = word.substring(0,3)
-                    const refinement = word[word.length - 1].replace('i','int').replace('f','flaw').replace('r','rad')
-                    if (!main_refinements.includes(refinement)) main_refinements.push(refinement)
-                }
-                else if (['random','randoms','trace','traces'].includes(word)) {
-                    if (!main_relics.includes(convertUpper(word))) main_relics.push(convertUpper(word))
-                }
-                else if ((word.length == 2 || word.length == 3) && !Number(word[0]) && Number(`${word[1]}${word[2] || ''}`)) {
-                    if (!main_relics.includes(word.toUpperCase())) main_relics.push(word.toUpperCase())
-                }
-                else if (word.match('cycle') || word.match('cycles')) {
-                    const prev_word = subline[0].split(' ')[index-1]
-                    cycle_count = prev_word
-                }
-                else if (word == 'steelpath' || word == 'sp') is_steelpath = true
-                else if (word == 'railjack' || word == 'rj') is_railjack = true
-            });
-            if (main_relics.length == 0) return resolve({
+            if (squad.main_relics.length == 0) return resolve({
                 code: 400,
                 message: `Relic name could not be determined`
             })
-            subline[1]?.split(' ').forEach((word,index) => {
-                if (['int','flaw','rad','intact','flawless','radiant'].includes(word)) {
-                    word = word.replace('intact','int').replace('flawless','flaw').replace('radiant','rad')
-                    if (!off_refinements.includes(word)) off_refinements.push(word)
-                }
-                else if ((word.length == 2 || word.length == 3) && !Number(word[0]) && Number(`${word[1]}${word[2] || ''}`)) {
-                    if (!off_relics.includes(word.toUpperCase())) off_relics.push(word.toUpperCase())
-                }
-                else if (word.match('cycle') || word.match('cycles')) {
-                    const prev_word = subline[1].split(' ')[index-1]
-                    cycle_count = prev_word
-                }
-                else if (word == 'steelpath' || word == 'sp') is_steelpath = true
-                else if (word == 'railjack' || word == 'rj') is_railjack = true
-            });
-
-            if (squad_type == '') squad_type = '4b4'
-            if (main_refinements.length == 0) main_refinements.push('rad')
-            if (is_steelpath && is_railjack) is_railjack = false
-
-            /*const rb_squads = await db.query(`SELECT * FROM rb_squads WHERE tier = '${tier}'`).catch(console.error)
-
-            if (rb_squads.rowCount > 5) return resolve({
-                code: 400,
-                message: `${tier} squads limit has been reached. Please try hosting later or join an existing squad`
-            })*/
+            if (squad.squad_type == '') squad.squad_type = '4b4'
+            if (squad.main_refinements.length == 0) squad.main_refinements.push('rad')
+            if (squad.is_steelpath && squad.is_railjack) squad.is_railjack = false
 
             db.query(`INSERT INTO rb_squads (squad_id,tier,members,original_host,main_relics,main_refinements,off_relics,off_refinements,squad_type,cycle_count,is_steelpath,is_railjack,creation_timestamp,joined_from_channel_ids) 
             VALUES 
                 (
-                (SELECT CASE WHEN (COUNT(squad_id) >= 5) THEN NULL ELSE '${squad_id}'::uuid END AS counted FROM rb_squads WHERE tier='${tier}' AND status='active'),
-                '${tier}',
+                (SELECT CASE WHEN (COUNT(squad_id) >= 5) THEN NULL ELSE '${squad_id}'::uuid END AS counted FROM rb_squads WHERE tier='${squad.tier}' AND status='active'),
+                '${squad.tier}',
                 '["${data.discord_id}"]',
                 '${data.discord_id}',
-                '${JSON.stringify(main_relics)}',
-                '${JSON.stringify(main_refinements)}',
-                '${JSON.stringify(off_relics)}',
-                '${JSON.stringify(off_refinements)}',
-                '${squad_type}',
-                '${cycle_count}',
-                ${is_steelpath},
-                ${is_railjack},
+                '${JSON.stringify(squad.main_relics)}',
+                '${JSON.stringify(squad.main_refinements)}',
+                '${JSON.stringify(squad.off_relics)}',
+                '${JSON.stringify(squad.off_refinements)}',
+                '${squad.squad_type}',
+                '${squad.cycle_count}',
+                ${squad.is_steelpath},
+                ${squad.is_railjack},
                 ${new Date().getTime()},
                 '${data.channel_id ? `{"${data.discord_id}":"${data.channel_id}"}`:'{}'}')
             `).then(res => {
@@ -210,7 +152,6 @@ function squadsUpdate(data,callback) {
         })
     }).catch(console.error)
 }
-
 
 function squadsAddMember(data,callback) {
     console.log('[squadsAddMember] data:',data)
@@ -285,6 +226,140 @@ function squadsLeaveAll(data,callback) {
     })
 }
 
+function trackersCreate(data,callback) {
+    console.log('[trackersCreate] data:',data)
+    if (!data.message) return callback({code: 400, err: 'No message provided'})
+    if (!data.discord_id) return callback({code: 400, err: 'No discord_id provided'})
+    if (!data.channel_id) return callback({code: 400, err: 'No channel_id provided'})
+    const lines = data.message.toLowerCase().trim().split('\n')
+    Promise.all(lines.map(line => {
+        return new Promise((resolve,reject) => {
+            const tracker_id = uuid.v4()
+            const squad = relicBotStringToSquad(line)
+            if (!['lith','meso','neo','axi'].includes(squad.tier)) return resolve({
+                code: 400,
+                message: `Invalid tier **${tier}**`
+            })
+            if (squad.main_relics.length == 0) return resolve({
+                code: 400,
+                message: `Relic name could not be determined`
+            })
+            if (squad.squad_type == '') squad.squad_type = '4b4'
+            if (squad.main_refinements.length == 0) squad.main_refinements.push('rad')
+            if (squad.is_steelpath && squad.is_railjack) squad.is_railjack = false
+
+            db.query(`INSERT INTO rb_trackers (tracker_id,discord_id,channel_id,tier,main_relics,main_refinements,off_relics,off_refinements,squad_type,cycle_count,is_steelpath,is_railjack) 
+            VALUES (
+                '${tracker_id}',
+                '${data.discord_id}',
+                '${data.channel_id}',
+                '${squad.tier}',
+                '${JSON.stringify(squad.main_relics)}',
+                '${JSON.stringify(squad.main_refinements)}',
+                '${JSON.stringify(squad.off_relics)}',
+                '${JSON.stringify(squad.off_refinements)}',
+                '${squad.squad_type}',
+                '${squad.cycle_count}',
+                ${squad.is_steelpath},
+                ${squad.is_railjack}
+            )`).then(res => {
+                if (res.rowCount == 1) {
+                    return resolve({
+                        code: 200
+                    })
+                } else return resolve({
+                    code: 500,
+                    message: 'unexpected db response'
+                })
+            }).catch(err => {
+                console.log(err)
+                return resolve({
+                    code: 500,
+                    message: err.stack
+                })
+            })
+        })
+    })).then(res => {
+        return callback(res)
+    }).catch(err => {
+        console.log(err)
+        return callback({
+            code: 500,
+            message: err.stack
+        })
+    })
+}
+
+function trackersFetch(data,callback) {
+    console.log('[trackersFetch] data:',data)
+    if (!data.discord_id) return callback({code: 500, err: 'No discord_id provided'})
+    db.query(`
+        SELECT * FROM rb_trackers WHERE discord_id='${data.discord_id}';
+    `).then(res => {
+        return callback({
+            code: 200,
+            data: res.rows
+        })
+    }).catch(err => {
+        console.log(err)
+        return callback({
+            code: 500,
+            message: err.stack
+        })
+    })
+}
+
+function trackersfetchSubscribers(data,callback) {
+    console.log('[trackersfetchSubscribers] data:',data)
+    if (!data.squad) return callback({code: 500, err: 'No squad obj provided'})
+    const squad = data.squad
+    db.query(`SELECT * FROM rb_trackers WHERE discord_id != '${squad.original_host}';`)
+    .then(res => {
+        const channel_ids = {};
+        const hosted_squad = relicBotSquadToString(squad,true)
+        res.rows.forEach(tracker => {
+            if (hosted_squad == relicBotSquadToString(tracker,true)) {
+                if (!channel_ids[tracker.channel_id]) channel_ids[tracker.channel_id] = []
+                channel_ids[tracker.channel_id].push(tracker.discord_id)
+            }
+        })
+        return callback({
+            code: 200,
+            data: channel_ids
+        })
+    }).catch(err => {
+        console.log(err)
+        return callback({
+            code: 500,
+            message: err.stack
+        })
+    })
+}
+
+function trackersDelete(data,callback) {
+    console.log('[trackersDelete] data:',data)
+    if (!data.discord_id && !data.tracker_ids) return callback({code: 500, err: 'No discord_id or tracker_ids provided'})
+    var query = ''
+    if (data.discord_id) {
+        query = `DELETE FROM rb_trackers WHERE discord_id='${data.discord_id}';`
+    } else {
+        data.tracker_ids.forEach(tracker_id => {
+            query += `DELETE FROM rb_trackers WHERE tracker_id='${tracker_id}';`
+        })
+    }
+    db.query(query).then(res => {
+        return callback({
+            code: 200,
+        })
+    }).catch(err => {
+        console.log(err)
+        return callback({
+            code: 500,
+            message: err.stack
+        })
+    })
+}
+
 function usersFetch(data,callback) {
     console.log('[usersFetch] data:',data)
     db.query(`
@@ -301,6 +376,75 @@ function usersFetch(data,callback) {
             message: err.stack
         })
     })
+}
+
+function relicBotStringToSquad(str) {
+    var squad = {
+        tier: '',
+        main_relics: [],
+        main_refinements: [],
+        off_relics: [],
+        off_refinements: [],
+        squad_type: '',
+        cycle_count: '',
+        is_steelpath: false,
+        is_railjack: false
+    }
+    str = str.toLowerCase().trim()
+    str = str.replace(/^h /,'').replace(/off$/g,'').replace(/off$/g,'').replace(/offcycle$/g,'').replace(/ or /g,'')
+    .replace(/^random lith/,'lith random').replace(/^random meso/,'meso random').replace(/^random neo/,'neo random').replace(/^random axi/,'axi random')
+    .replace(/^randoms lith/,'lith random').replace(/^randoms meso/,'meso random').replace(/^randoms neo/,'neo random').replace(/^randoms axi/,'axi random')
+    .replace(/^trace lith/,'lith trace').replace(/^trace meso/,'meso trace').replace(/^trace neo/,'neo trace').replace(/^trace axi/,'axi trace')
+    .replace(/^traces lith/,'lith trace').replace(/^traces meso/,'meso trace').replace(/^traces neo/,'neo trace').replace(/^traces axi/,'axi trace');
+    squad.tier = str.split(' ')[0]
+    str = str.replaceAll(squad.tier,'').replace(/,/g,' ').replace(/\s+/g, ' ').trim()
+    const subline = str.split(' with ')
+
+    subline[0].split(' ').forEach((word,index) => {
+        if (['int','flaw','rad','intact','flawless','radiant'].includes(word)) {
+            word = word.replace('intact','int').replace('flawless','flaw').replace('radiant','rad')
+            if (!squad.main_refinements.includes(word)) squad.main_refinements.push(word)
+        }
+        else if (['2b2','4b4'].includes(word)) {
+            squad.squad_type = word
+        }
+        else if (['2b2i','4b4i','2b2f','4b4f','2b2r','4b4r',].includes(word)) {
+            squad.squad_type = word.substring(0,3)
+            const refinement = word[word.length - 1].replace('i','int').replace('f','flaw').replace('r','rad')
+            if (!squad.main_refinements.includes(refinement)) squad.main_refinements.push(refinement)
+        }
+        else if (['random','randoms','trace','traces'].includes(word)) {
+            if (!squad.main_relics.includes(convertUpper(word))) squad.main_relics.push(convertUpper(word))
+        }
+        else if ((word.length == 2 || word.length == 3) && !Number(word[0]) && Number(`${word[1]}${word[2] || ''}`)) {
+            if (!squad.main_relics.includes(word.toUpperCase())) squad.main_relics.push(word.toUpperCase())
+        }
+        else if (word.match('cycle') || word.match('cycles')) {
+            const prev_word = subline[0].split(' ')[index-1]
+            squad.cycle_count = prev_word
+        }
+        else if (word == 'steelpath' || word == 'sp') squad.is_steelpath = true
+        else if (word == 'railjack' || word == 'rj') squad.is_railjack = true
+    });
+    subline[1]?.split(' ').forEach((word,index) => {
+        if (['int','flaw','rad','intact','flawless','radiant'].includes(word)) {
+            word = word.replace('intact','int').replace('flawless','flaw').replace('radiant','rad')
+            if (!squad.off_refinements.includes(word)) squad.off_refinements.push(word)
+        }
+        else if ((word.length == 2 || word.length == 3) && !Number(word[0]) && Number(`${word[1]}${word[2] || ''}`)) {
+            if (!squad.off_relics.includes(word.toUpperCase())) squad.off_relics.push(word.toUpperCase())
+        }
+        else if (word.match('cycle') || word.match('cycles')) {
+            const prev_word = subline[1].split(' ')[index-1]
+            squad.cycle_count = prev_word
+        }
+        else if (word == 'steelpath' || word == 'sp') squad.is_steelpath = true
+        else if (word == 'railjack' || word == 'rj') squad.is_railjack = true
+    });
+    return squad;
+}
+function relicBotSquadToString(squad,include_sp_rj) {
+    return `${convertUpper(squad.tier)} ${squad.main_relics.join(' ')} ${squad.squad_type} ${squad.main_refinements.join(' ')} ${squad.off_relics.length > 0 ? 'with':''} ${squad.off_relics.join(' ')} ${squad.off_refinements.join(' ')} ${include_sp_rj ? (squad.is_steelpath ? 'Steelpath':squad.is_railjack ? 'Railjack':''):''} ${squad.cycle_count == '' ? '':`(${squad.cycle_count} cycles)`}`.replace(/\s+/g, ' ').trim()
 }
 
 module.exports = {
