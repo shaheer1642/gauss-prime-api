a6da61d96d74de11639859b5579960ce428ac0f6
MrSofty123 <shaheer1642@gmail.com>
2022-12-11 01:16:40 +0500

    Update relicbot.js

--- modules/relicbot.js
+++ modules/relicbot.js
@@ -479,6 +479,7 @@ function statsFetch(data,callback) {
             stats.push(users_list[discord_id])
         }
         stats = stats.sort(dynamicSortDesc("squads_completed"))
+        stats = stats.stats.map((stat,index) => index < 10 ? stat:null).filter(o => o != null)
         console.log(stats)
         return callback({
             code: 200,
