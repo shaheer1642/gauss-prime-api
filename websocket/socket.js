38ef1967ff62122b9de9e4bbce89882e7403313f
MrSofty123 <shaheer1642@gmail.com>
2022-12-26 08:08:20 +0500

    Update socket.js

--- websocket/socket.js
+++ websocket/socket.js
@@ -1,9 +1,7 @@
 const {server} = require('../api/api')
 const { Server } = require("socket.io");
 const io = new Server(server, {
-  transports: ['websocket'],
-  pingInterval: 1000 * 60 * 5,
-  pingTimeout: 1000 * 60 * 3
+  transports: ['websocket']
 });
 const {db} = require('../modules/db_connection')
 const uuid = require('uuid');
