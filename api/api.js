56918153ebbffb4d2f791622a19b73e3c77e2146
MrSofty123 <shaheer1642@gmail.com>
2023-04-21 05:47:17 +0500

    wfrim upload endpoint

--- api/api.js
+++ api/api.js
@@ -277,6 +277,34 @@ app.get('/api/allsquads/authenticate', async (req, res) => {
   })
 })
 
+app.post('/api/wfrim/uploadrelicsdb', async (req,res) => {
+  console.log('[/api/wfrim/uploadrelicsdb] username=',req.body.username)
+  if (!req.body.username) return
+  db.query(`
+    UPDATE wfrim_relicsdb
+    SET data = '${JSON.stringify(req.body.data)}', timestamp = ${new Date().getTime()}
+    WHERE username = '${req.body.username}'
+  `).then(dbres => {
+    if (dbres.rowCount == 0) {
+      db.query(`
+        INSERT INTO wfrim_relicsdb 
+        (username, data, timestamp)
+        VALUES
+        ('${req.body.username}','${JSON.stringify(req.body.data)}',${new Date().getTime()})
+      `).then(dbres => {
+        if (dbres.rowCount == 1) return res.send({code: 200})
+        else return res.send({code: 500})
+      }).catch(err => {
+        console.log(err)
+        res.send({code: 500})
+      })
+    }
+  }).catch(err => {
+    console.log(err)
+    res.send({code: 500})
+  })
+})
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
 });
