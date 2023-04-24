d8bf8eb081ac2561a7189d898b3218671e6172eb
MrSofty123 <shaheer1642@gmail.com>
2023-04-24 19:31:46 +0500

    changes

--- modules/allsquads/as_users_list.js
+++ modules/allsquads/as_users_list.js
@@ -3,6 +3,7 @@ const {event_emitter} = require('../event_emitter')
 const JSONbig = require('json-bigint');
 
 var as_users_list = {}
+var as_users_list_discord = {}
 
 event_emitter.on('db_connected', () => {
     updateUsersList()
@@ -13,6 +14,7 @@ function updateUsersList() {
     db.query(`SELECT * FROM as_users_list`).then(res => {
         res.rows.forEach(row => {
             as_users_list[row.user_id] = row
+            if (row.discord_id) as_users_list_discord[row.discord_id] = row
         })
         console.log('[as_users_list.updateUsersList] finished')
     }).catch(console.error)
@@ -23,13 +25,10 @@ function updateUser(user_id) {
     db.query(`SELECT * FROM as_users_list WHERE user_id = '${user_id}'`).then(res => {
         res.rows.forEach(row => {
             as_users_list[row.user_id] = row
+            if (row.discord_id) as_users_list_discord[row.discord_id] = row
         })
         console.log('[as_users_list.updateUser] finished')
     }).catch(console.error)
-}
-
-function getAsUserByDiscordId(discord_id) {
-    return Object.values(as_users_list).filter(user => user.discord_id == discord_id)?.[0]
 }
 
 db.on('notification',(notification) => {
@@ -41,5 +40,5 @@ db.on('notification',(notification) => {
 
 module.exports = {
     as_users_list,
-    getAsUserByDiscordId
+    as_users_list_discord
 }
